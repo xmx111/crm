105b2a8b1f840e32bda276c1155353dfe345af55
xmx111 <xmx111@gmail.com>
2013-07-22 23:40:38 +0800

    GET传值

--- server.py
+++ server.py
@@ -1,13 +1,19 @@
 #-*-coding:utf-8-*-
 import web
 urls = (
-    '/', 'index'
+    '/', 'index',
+    '/list/(.+)', 'list_user'
     )
 render = web.template.render('template/')
 class index:
     def GET(self):
         #传值
         return render.index('老婆', '哈哈')
+
+class list_user:
+    def GET(self, name):
+        #GET传值
+        return "get name value: {0}".format(name)
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
