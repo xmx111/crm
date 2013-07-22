77811e11b705ba09be527ada7324be6d16b77c56
xmx111 <xmx111@gmail.com>
2013-07-22 22:44:10 +0800

    修改文字

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ render = web.template.render('template/')
 class index:
     def GET(self):
         #传值
-        return render.first('parm1', 'parm2')
+        return render.index('老婆', '哈哈')
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
